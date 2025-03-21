ec6ac73533e7af8eaf504181ddb0b5314bf52500
ganesh-91 <ganesh.khaire91@gmail.com>
2025-03-21 23:43:29 +0530

    function takes argument in string and return number

--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -5,5 +5,9 @@ describe("String Calculator", () => {
     test("returns 0 for an empty string", () => {
       expect(add("")).toBe(0);
     });
+
+    test("returns number for an number string", () => {
+      expect(add("1")).toBe(1);
+    });
   });
 });
